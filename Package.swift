85301df7235454318fc1d94226df624860c222ef
Dmitry Kulakov <navartis@gmail.com>
2023-06-14 22:55:27 +0300

    Add Tuist compatibility

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Wormholy",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Wormholy",
+            type: .dynamic,
             targets: ["WormholyObjC"]),
     ],
     dependencies: [
